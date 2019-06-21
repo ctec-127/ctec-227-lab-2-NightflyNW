6718bcb158bea3a9871ab4d95bad085e66d75e88
Scott Altom <s.altom@students.clark.edu>
2019-06-21 10:17:48 -0700

    Completed Lab

---
+++ cookie_eat.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cookie Eat</title>
+</head>
+<body>
+<?php
+
+# echo the value of each cookie using $_COOKIE[] 
+if (isset($_COOKIE['cookie'])) {
+    foreach ($_COOKIE['cookie'] as $name => $value) {
+        $name = htmlspecialchars($name);
+        $value = htmlspecialchars($value);
+        echo "$name : $value <br />\n";
+    }
+}
+?>
+    
+ 
+</body>
+</html>
+
